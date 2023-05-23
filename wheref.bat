57bcfbe7a3136cbf0389599177eddfaa25f598c9
Lim <1201101815@student.mmu.edu.my>
2023-05-23 20:17:34 +0800

    cleanup code

--- wheref.bat
+++ wheref.bat
@@ -1,11 +1,7 @@
 @echo off
-setlocal enabledelayedexpansion
-
 if "%2"=="" (
     cd C:\ && set folderName=%1
     ) else (
         cd %1 && set folderName=%2
     )
-
 dir /o /b /a:d /s | findstr /e /i "%folderName%"
-pause
