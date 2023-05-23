5b81d0c823473c34f0f612eff087513e5d92b43d
Lim <1201101815@student.mmu.edu.my>
2023-05-23 20:11:50 +0800

    working script

--- wheref.bat
+++ wheref.bat
@@ -1,15 +1,11 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-set "search_folder=C:\Path\To\Start\Search"
-set "target_folder=FolderNameToFind"
-
-if %2=="" (set "search_folder=C:\" && set "folderName=%1") else (set "search_folder=%1" && set "folderName=%2")
-
-for /r "%search_folder%" %%G in (.) do (
-    if /i "%%~nxG"=="%target_folder%" (
-        echo %%~dpG
+if "%2"=="" (
+    cd C:\ && set folderName=%1
+    ) else (
+        cd %1 && set folderName=%2
     )
-)
 
-endlocal
+dir /o /b /a:d /s | findstr /e /i "%folderName%"
+pause
